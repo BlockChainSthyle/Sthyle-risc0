cb5da7c003323ba6396aa4d2ad4bbbadacbdd566
ericsaikali <eric@mikaw.net>
2025-03-08 12:58:40 +0100

    Add hex lib

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -1,66 +1,73 @@
-use borsh::{io::Error, BorshDeserialize, BorshSerialize};
+use borsh::{BorshDeserialize, BorshSerialize};
 use serde::{Deserialize, Serialize};
-
 use sdk::{Digestable, HyleContract, RunResult};
 
-impl HyleContract for Counter {
-    /// Entry point of the contract's logic
-    fn execute(&mut self, contract_input: &sdk::ContractInput) -> RunResult {
-        // Parse contract inputs
-        let (action, ctx) = sdk::utils::parse_raw_contract_input::<CounterAction>(contract_input)?;
+/// Define contract actions for image authentication
+#[derive(BorshSerialize, BorshDeserialize, Debug, Clone)]
+pub enum ImageAction {
+    RegisterImage { hash: Vec<u8>, owner: String },
+    VerifyImage { hash: Vec<u8> },
+    RegisterEdit { original_hash: Vec<u8>, edited_hash: Vec<u8>, owner: String },
+}
 
-        // Execute the contract logic
-        match action {
-            CounterAction::Increment => self.value += 1,
+/// The state of the contract, storing registered images and edits
+#[derive(BorshSerialize, BorshDeserialize, Serialize, Deserialize, Debug, Clone)]
+pub struct ImageRegistry {
+    pub images: Vec<(Vec<u8>, String)>, // (image_hash, owner)
+    pub edits: Vec<(Vec<u8>, Vec<u8>, String)>, // (original_hash, edited_hash, owner)
+}
+
+impl Default for ImageRegistry {
+    fn default() -> Self {
+        Self {
+            images: Vec::new(),
+            edits: Vec::new(),
         }
+    }
+}
 
-        // program_output might be used to give feedback to the user
-        let program_output = format!("new value: {}", self.value);
+impl HyleContract for ImageRegistry {
+    fn execute(&mut self, contract_input: &sdk::ContractInput) -> RunResult {
+        let (action, ctx) = sdk::utils::parse_raw_contract_input::<ImageAction>(contract_input)?;
+
+        let program_output = match action {
+            ImageAction::RegisterImage { hash, owner } => {
+                if !self.images.iter().any(|(h, _)| *h == hash) {
+                    self.images.push((hash.clone(), owner.clone()));
+                    format!("Image registered by {}", owner)
+                } else {
+                    "Image already registered".to_string()
+                }
+            }
+            ImageAction::VerifyImage { hash } => {
+                if let Some((_, owner)) = self.images.iter().find(|(h, _)| *h == hash) {
+                    format!("Image is authentic. Owner: {}", owner)
+                } else {
+                    "Image not found".to_string()
+                }
+            }
+            ImageAction::RegisterEdit { original_hash, edited_hash, owner } => {
+                if self.images.iter().any(|(h, _)| *h == original_hash) {
+                    self.edits.push((original_hash.clone(), edited_hash.clone(), owner.clone()));
+                    format!("Edit registered by {}", owner)
+                } else {
+                    "Original image not found".to_string()
+                }
+            }
+        };
+
         Ok((program_output, ctx, vec![]))
     }
 }
 
-/// The action represents the different operations that can be done on the contract
-#[derive(BorshSerialize, BorshDeserialize, Debug, Clone)]
-pub enum CounterAction {
-    Increment,
-}
-
-/// The state of the contract, in this example it is fully serialized on-chain
-#[derive(BorshSerialize, BorshDeserialize, Serialize, Deserialize, Debug, Clone)]
-pub struct Counter {
-    pub value: u32,
-}
-
-/// Utils function for the host
-impl Counter {
-    pub fn as_bytes(&self) -> Result<Vec<u8>, Error> {
-        borsh::to_vec(self)
-    }
-}
-
-/// Utils function for the host
-impl CounterAction {
-    pub fn as_blob(&self, contract_name: &str) -> sdk::Blob {
-        sdk::Blob {
-            contract_name: contract_name.into(),
-            data: sdk::BlobData(borsh::to_vec(self).expect("failed to encode BlobData")),
-        }
-    }
-}
-
-/// Helpers to transform the contrat's state in its on-chain state digest version.
-/// In an optimal version, you would here only returns a hash of the state,
-/// while storing the full-state off-chain
-impl Digestable for Counter {
+impl Digestable for ImageRegistry {
     fn as_digest(&self) -> sdk::StateDigest {
-        sdk::StateDigest(borsh::to_vec(self).expect("Failed to encode Balances"))
+        sdk::StateDigest(borsh::to_vec(self).expect("Failed to encode ImageRegistry"))
     }
 }
-impl From<sdk::StateDigest> for Counter {
+
+impl From<sdk::StateDigest> for ImageRegistry {
     fn from(state: sdk::StateDigest) -> Self {
-        borsh::from_slice(&state.0)
-            .map_err(|_| "Could not decode hyllar state".to_string())
-            .unwrap()
+        borsh::from_slice(&state.0).unwrap_or_default()
     }
 }
